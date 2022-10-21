873852f7725dd49feccff3a495550483af419b58
Pragasen Naicker <pnaicker.dev@gmail.com>
2022-10-21 16:04:12 +0200

    added auth

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import { Amplify } from "aws-amplify";
+import awsmobile from "./aws-exports";
+Amplify.configure(awsmobile);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
